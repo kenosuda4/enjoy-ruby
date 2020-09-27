4da51c7ffd739c693ba08bdf9535aa95cfe41ed3
清水龍之介 <rs7ten@gmail.com>
2020-09-27 23:53:21 +0900

    chapter-5 finish

--- chapter-5/unless.rb
+++ chapter-5/unless.rb
@@ -1,5 +1,21 @@
+# unlrssは条件が偽(false)の時に処理が実行される
+
 a = 10
 b = 20
 unless a > b
     puts "aよりbはちいさい"
+end
+
+=begin
+ unless文の条件　a > bが偽になるため
+ putsメソッドが実行される
+ unless文でもelseが使用できる
+=end
+
+a = 10
+b = 9
+unless a > b
+    puts "aよりbはちいさい"
+else
+    puts "aはbよりちいさい"
 end
