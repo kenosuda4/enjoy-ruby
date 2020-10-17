4e3de05928f8c64b7b9259d746f1fc7d853a4730
清水龍之介 <rs7ten@gmail.com>
2020-10-17 20:58:57 +0900

    max

--- chapter-7/7.3.2.rb
+++ chapter-7/7.3.2.rb
@@ -3,3 +3,23 @@ def volume(x, y, z)
 end
 
 p volume(2, 3, 4)
+p volume(10, 20, 30)
+
+def area(x, y, z)
+    xy = x * y
+    yz = y * z
+    zx = z * xy
+end
+
+p area(2, 3, 4)     # 結果 24
+p area(10, 20, 30)  #結果 6000
+
+def max(a, b)
+    if a > b
+        a    
+    else
+         b
+    end
+end
+
+p max(10, 5)
