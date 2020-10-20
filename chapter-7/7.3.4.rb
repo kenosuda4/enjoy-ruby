83844220d189bba6fa91123f277158c5265630ba
清水龍之介 <rs7ten@gmail.com>
2020-10-20 16:36:35 +0900

    7.3.5

--- chapter-7/7.3.4.rb
+++ chapter-7/7.3.4.rb
@@ -1,5 +1,23 @@
+# ※1
 def foo(*args)
     args
 end
 
 p foo(1, 2, 3)
+
+# ※2
+def math(arg, *args)
+    [arg, args]
+end
+
+p math(1)
+p math(1, 2, 3)
+
+
+# ※3
+def a(a, *b, c)
+    [a, b, c]
+end
+
+p a(1, 2, 3, 4, 5)
+p a(1, 2)
