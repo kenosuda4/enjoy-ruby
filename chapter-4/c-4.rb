c9431a5c969c7ba72b69589536f9baa607718d88
kenosuda4 <rs7ten@gmail.com>
2020-11-10 23:34:14 +0900

    Array

--- chapter-4/c-4.rb
+++ chapter-4/c-4.rb
@@ -6,9 +6,14 @@ first, second, third = 1, 2, 3
 p "#{first} #{second} #{third}"
 
 # 変数の値の入れ替え
-
 a, b = 1, 0
 tmp = a
 a = b
 b = tmp
 p [a, b]
+
+# 配列の要素の取り出し 3行目 配列の要素を多重代入
+ary = [1, 2, 3, 4]
+p ary[0]
+a, b, c, d = ary
+p b
